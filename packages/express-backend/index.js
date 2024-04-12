4759e49cf6a998cb74b1ac5229f212a9a523bf2d
Ryan Vo <38389871+ryyyvo@users.noreply.github.com>
2024-04-12 00:59:00 -0700

    Create backend directory and index.js

---
+++ packages/express-backend/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+const port = 8000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Hello world!");
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
